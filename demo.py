150fe83f6b855b34dcfef12d0be450b0c79e7068
KievTrung <kieutrung0701@gmail.com>
2025-05-25 07:31:37 +0700

    update demo.py

--- demo.py
+++ demo.py
@@ -1,21 +1,32 @@
 from datasets import Dataset
 from gpt2_vs_bert_rc_squadv2.inferencing import QaModel
 
-if __name__ == '__main__':
-    # sample dataset 
+if __name__ == "__main__":
+    # sample dataset
     data = {
         "id": ["1", "2", "3"],
         "context": [
             "The sky is blue.",
             "Water boils at 100 degrees Celsius.",
-            "Python is a programming language."
+            "Python is a programming language.",
         ],
         "question": [
             "What color is the sky?",
             "At what temperature does water boil?",
-            "What is Python?"
-        ]
+            "What is Python?",
+        ],
     }
     samples = Dataset.from_dict(data)
     model = QaModel()
-    model.pipeline(samples)
+    result = model.pipeline(samples)
+
+"""
+    result has this format:
+    result = { 'id':{
+            "context": str,
+            "question": str,
+            "true answers":{'text': str, 'answer_start':int} 
+            "pred": list 
+        } 
+    }
+"""
